fc5fdf473a3b650e8814670fdceca1beec3216c9
emran <emranhi001@gmail.com>
2023-09-09 12:19:16 +0300

    first commit

---
+++ names.js
@@ -0,0 +1,9 @@
+
+//global
+const emran = "emran"
+const ammar = "ammar"
+module.exports= {ammar, emran}
+
+//   require("./node")("ammer")
+ 
+ 
---
+++ node.js
@@ -0,0 +1,7 @@
+// setInterval(function, time)
+const sayHi = (name)=> {
+    console.log("helo "+name)
+}
+const ammar = require("./names")
+module.exports = sayHi
+
